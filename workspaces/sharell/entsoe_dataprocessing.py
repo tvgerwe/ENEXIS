a09eb0568907cd56e4b5769d80b2d3c66b1f3db5
sharell-beep <sharellbas@gmail.com>
2025-05-02 16:25:59 +0200

    entsoe data processing

--- workspaces/sharell/entsoe_dataprocessing.py
+++ workspaces/sharell/entsoe_dataprocessing.py
@@ -1,17 +1,25 @@
 import pandas as pd
 from entsoe import EntsoePandasClient
 import time
+import sqlite3
 
-# Load the existing CSV file
-df = pd.read_csv('electricity_data_nl_2022_2025.csv', index_col=0, parse_dates=True)
-print("CSV file loaded successfully!")
+# Pad naar de SQLite-database
+db_path = 'C:/Users/shba/Documents/ENEXIS/ENEXIS/src/data/WARP.db'
+
+# Maak verbinding met de database
+conn = sqlite3.connect(db_path)
+
+# Lees de 'raw_entsoe_obs'-tabel in een Pandas DataFrame
+query = "SELECT * FROM raw_entsoe_obs"
+df = pd.read_sql_query(query, conn)
+
+# Sluit de verbinding
+conn.close()
+
 
 # Ensure the index is datetime with UTC
 df.index = pd.to_datetime(df.index, utc=True)
 print("Index converted to datetime with UTC!")
-
-# Save the utc data to a new CSV file
-df.to_csv('electricity_data_nl_2022_2025_utc.csv')
 
 # Shift timestamps by 1 hour to calculate the mean for the past hour
 df.index = df.index - pd.Timedelta(hours=1)
@@ -20,14 +28,24 @@ df.index = df.index - pd.Timedelta(hours=1)
 df_hourly = df.resample('h').mean()
 print("Data resampled to hourly frequency based on the past hour!")
 
-# Save the resampled data to a new CSV file
-df_hourly.to_csv('electricity_data_nl_2022_2025_hourly.csv')
-print("Hourly data with flow saved successfully!")
-
 neighboring_countries = ['BE', 'DE', 'GB', 'DK', 'NO']
 
 for neighbor in neighboring_countries:
     df_hourly[f'Flow_{neighbor}'] = df_hourly[f'Flow_{neighbor}_to_NL'] - df_hourly[f'Flow_NL_to_{neighbor}']
 
 df_hourly['Total_Flow'] = df_hourly['Flow_BE'] + df_hourly['Flow_DE'] + df_hourly['Flow_GB'] + df_hourly['Flow_DK'] + df_hourly['Flow_NO']
-df_hourly.to_csv('electricity_data_nl_2022_2025_hourly_flow.csv')
+
+
+
+# Connect to the SQLite database
+db_path = 'C:/Users/shba/Documents/ENEXIS/ENEXIS/src/data/WARP.db'
+conn = sqlite3.connect(db_path)
+
+# Write the DataFrame to the database table 'transformed_entsoe_obs'
+# If table exists, replace it. If not, create new table
+df_hourly.to_sql('transformed_entsoe_obs', conn, if_exists='replace', index=False)
+
+# Close the connection
+conn.close()
+
+print("Data successfully written to database table 'transformed_entsoe_obs'")
