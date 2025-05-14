4c270664fe3b5b59d7d373a26ae5d4b4ccc59e1e
Twan van Gerwe <twanvangerwe@icloud.com>
2025-05-14 11:28:55 +0200

    sync issue solve

--- .history/src/data_processing/transform_NED_preds_20250507163657.py
+++
@@ -1,3 +0,0 @@
-df_NED_preds_CSV['validfrom'] = pd.to_datetime(df['validfrom'])
-df_NED_preds_CSV['validto'] = pd.to_datetime(df['validto'])
-df_NED_preds_CSV['lastupdate'] = pd.to_datetime(df['lastupdate'])
