6456c2de1abfd26998059325ca29841e6cc6f2c1
Twan van Gerwe <twanvangerwe@icloud.com>
2025-05-19 11:03:51 +0200

    sync issue solved?

--- workspaces/sandeep/deploy/app.py
+++
@@ -1,7 +0,0 @@
-import gradio as gr
-from agentic_ai import agentic_ai
-
-def run_agent(prompt):
-    return agentic_ai(prompt)
-
-gr.Interface(fn=run_agent, inputs="text", outputs="text", title="Agentic AI Prompt Runner").launch()
